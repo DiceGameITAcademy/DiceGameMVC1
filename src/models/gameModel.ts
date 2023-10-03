298b84da0dce5d3efd0a8b274c46f827da43beb6
Ivan de Gea <92228308+IvandeGea@users.noreply.github.com>
2023-10-03 10:52:09 +0200

    añadiendo modelo game

---
+++ src/models/gameModel.ts
@@ -0,0 +1,59 @@
+import { DataTypes,Model, Sequelize } from 'sequelize';
+import  sequelize  from '../../config/sequelize.config';
+
+interface GameAttributes{
+	id: number;
+	diceValue1: number;
+	diceValue2: number;
+	result: number;
+	win: boolean;
+    playerId: number;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export interface GameInstance extends Model<GameAttributes>, GameAttributes {}
+
+export const GameDb = sequelize.define<GameInstance>('Games', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	diceValue1: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
+	diceValue2: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
+	result: {
+		type: DataTypes.INTEGER,
+		allowNull: false
+	},
+	win: {
+		type: DataTypes.BOOLEAN,
+		allowNull: false
+	},
+	createdAt: {
+		type: DataTypes.DATE,
+		defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+		allowNull: false
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+		defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+		allowNull: false
+	},
+	playerId: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'players',
+			key: 'id'
+		},
+		allowNull: false
+	}
+});
+
+GameDb.sync()
