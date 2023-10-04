253f210b4dcf91034c206f982cde3d277452e927
Ivan de Gea <92228308+IvandeGea@users.noreply.github.com>
2023-10-04 11:06:45 +0200

    arreglando generador id para que sea number

--- src/controllers/GameService.ts
+++ src/controllers/GameService.ts
@@ -1,4 +1,6 @@
+import { number } from 'zod';
 import { Game } from '../models/game'
+import { v4 as uuidv4 } from 'uuid'
 
 export const roll = () => {
 	return Math.floor(Math.random() * 6) + 1;
@@ -12,11 +14,15 @@ export const checkWin = (result: number) => {
      if (result === 7) return true  
         else return false
 }   
+export const GameId = (): number => {
+    const uuid = uuidv4().replace(/-/g, ''); // Eliminar guiones de la cadena UUID
+    return parseInt(uuid, 16); // Convertir la cadena hexadecimal a número
+  };
 
 export const playGame = (id:number): Game => {
     if(typeof id !== 'number') throw new Error('id must be a number'
     )
-	const gameId = Math.random() * 1000;
+	const gameId :number = GameId();
 	const diceValue1: number = roll();
 	const diceValue2: number = roll();
 	const result: number = calcResult(diceValue1,diceValue2);
