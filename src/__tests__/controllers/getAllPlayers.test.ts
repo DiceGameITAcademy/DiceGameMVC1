82fd90258062832e7ac633c13106c559c2259a05
OllinDesigns <cesarvergara@tutanota.com>
2023-10-04 20:06:55 +0200

    commit

--- src/__tests__/controllers/getAllPlayers.test.ts
+++ src/__tests__/controllers/getAllPlayers.test.ts
@@ -1,29 +1,32 @@
-// import { Request, Response } from "express";
-// import { getAllPlayers } from "../../controllers/PlayerControllerCheck";
-// import Player from "src/models/playerModelCheck";
+import { Request, Response } from "express";
+import { getAllPlayers } from "../../controllers/PlayerControllerCheck";
+import Player from "src/models/playerModelCheck";
 
-// describe('getAllPlayers', () => {
-//   it('should return a list of all players in the database', async () => {
-//     const mockPlayers = [
-//       { id: 1, name: 'Player 1', password: 'password1', wins: 0, losses: 0 },
-//       { id: 2, name: 'Player 2', password: 'password2', wins: 0, losses: 0 },
-//       { id: 3, name: 'Player 3', password: 'password3', wins: 0, losses: 0 }
-//     ];
+describe('getAllPlayers', () => {
+  it('should return a list of all players in the database', async () => {
+    const mockPlayers = [
+      { id: 1, name: 'Player 1', password: 'password1', wins: 0, losses: 0 },
+      { id: 2, name: 'Player 2', password: 'password2', wins: 0, losses: 0 },
+      { id: 3, name: 'Player 3', password: 'password3', wins: 0, losses: 0 }
+    ];
 
-//     Player.findAll = jest.fn().mockResolvedValue(mockPlayers);
+    Player.findAll = jest.fn().mockResolvedValue(mockPlayers);
 
-//     const req = {} as Request;
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn()
-//     } as unknown as Response;
-//     const next = jest.fn();
+    const req = {} as Request;
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    } as unknown as Response;
+    const next = jest.fn();
 
-//     await getAllPlayers(req, res, next);
+    await getAllPlayers(req, res, next);
 
-//     expect(Player.findAll).toHaveBeenCalledWith({});
-//     expect(res.status).toHaveBeenCalledWith(200);
-//     expect(res.json).toHaveBeenCalledWith({ players: mockPlayers });
-//     expect(next).not.toHaveBeenCalled();
-//   });
-// });
+    expect(Player.findAll).toHaveBeenCalledWith({});
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ players: mockPlayers });
+    expect(next).not.toHaveBeenCalled();
+  });
+});
+
+
+// src/__tests__/controllers/getAllPlayers.test.ts.
