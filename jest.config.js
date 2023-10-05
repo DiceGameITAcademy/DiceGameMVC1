942737557dfd54d77410c32cecf5380b08f691c2
OllinDesigns <cesarvergara@tutanota.com>
2023-10-05 13:26:45 +0200

    commit to main branch

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   cacheDirectory: '.tmp/jestCache',
+  silent: true,
   moduleNameMapper: {
     '^@server/(.*)$': '<rootDir>/server/src/$1',
   },
