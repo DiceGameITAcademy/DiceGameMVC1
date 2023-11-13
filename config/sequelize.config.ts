9758ac8ac6b9676e995d7fb922646c193736e7ce
OllinDesigns <cesarvergara@tutanota.com>
2023-11-13 19:45:08 +0100

    environment variables

--- config/sequelize.config.ts
+++ config/sequelize.config.ts
@@ -1,13 +1,34 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize } from 'sequelize';
+
+require('dotenv').config();
 
 const sequelize = new Sequelize({
-  database: "dicegame",
-  username: "root",
-  password: "password",
-  dialect: "mysql",
-  host: "localhost",
-  port: 3306,
-  logging: console.log,
+  database: process.env.DB_DATABASE as string,
+  username: process.env.DB_USERNAME as string,
+  password: process.env.DB_PASSWORD as string,
+  dialect: process.env.DB_DIALECT as 'mysql', 
+  host: process.env.DB_HOST as string,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306, 
+  logging: console.log as any, 
 });
 
 export default sequelize;
+
+
+
+
+
+
+// import { Sequelize } from "sequelize-typescript";
+
+// const sequelize = new Sequelize({
+//   database: "dicegame",
+//   username: "root",
+//   password: "password",
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   logging: console.log,
+// });
+
+// export default sequelize;
