b696eb0f95bdb555f0812df0a34cd9de76915330
OllinDesigns <cesarvergara@tutanota.com>
2023-11-13 19:49:11 +0100

    more env

--- config/sequelize.config.ts
+++ config/sequelize.config.ts
@@ -13,22 +13,3 @@ const sequelize = new Sequelize({
 });
 
 export default sequelize;
-
-
-
-
-
-
-// import { Sequelize } from "sequelize-typescript";
-
-// const sequelize = new Sequelize({
-//   database: "dicegame",
-//   username: "root",
-//   password: "password",
-//   dialect: "mysql",
-//   host: "localhost",
-//   port: 3306,
-//   logging: console.log,
-// });
-
-// export default sequelize;
